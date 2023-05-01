2747d2d42d899dc05f15a05f188fa92cf79d996d
Arqam <arqam401@gmail.com>
2023-05-01 09:03:26 +0500

    Write Function addBookToLibrary

--- script.js
+++ script.js
@@ -8,3 +8,12 @@ function Book(title, author, pages, read) {
     this.read = read;
 }
 
+function addBookToLibrary() {
+    let title = prompt("Title: ");
+    let author = prompt("Author: ");
+    let pages = prompt("Pages: ");
+    let read = prompt("Read: ");
+
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
